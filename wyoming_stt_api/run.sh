970fd148e9b894c5e18910814ab9741738d4600c
Gabriel Wong <gabrielwong159@gmail.com>
2025-09-20 23:23:18 +0800

    add config for max audio transcription duration
    We added a setting to limit the maximum audio duration sent to the OpenAI API
    for transcription. Make this setting configurable in the addon as well.

--- wyoming_stt_api/run.sh
+++ wyoming_stt_api/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 OPENAI_API_KEY=$(bashio::config 'openai_api_key')
-python -m wyoming_stt_api --openai-api-key "$OPENAI_API_KEY"
+MAX_AUDIO_DURATION_S=$(bashio::config 'max_audio_duration_s')
+
+python -m wyoming_stt_api \
+    --openai-api-key "$OPENAI_API_KEY" \
+    --max-audio-duration-s "$MAX_AUDIO_DURATION_S"
