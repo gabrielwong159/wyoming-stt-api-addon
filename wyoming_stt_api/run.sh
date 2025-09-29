2c9517e38aeb0ee7b9d29b8125cd8fc8e6554739
Gabriel Wong <gabrielwong159@gmail.com>
2025-09-30 00:30:49 +0800

    use env var for api key
    Update to v2.0.0 of `wyoming-stt-api`, which requires us to pass
    `OPENAI_API_KEY` as an env var, instead of a CLI arg.
    
    Bump the add-on itself to v2.0.0 to match `wyoming-stt-api`.

--- wyoming_stt_api/run.sh
+++ wyoming_stt_api/run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-OPENAI_API_KEY=$(bashio::config 'openai_api_key')
-MAX_AUDIO_DURATION_S=$(bashio::config 'max_audio_duration_s')
+export OPENAI_API_KEY=$(bashio::config 'openai_api_key')
+export MAX_AUDIO_DURATION_S=$(bashio::config 'max_audio_duration_s')
 
-python -m wyoming_stt_api \
-    --openai-api-key "$OPENAI_API_KEY" \
-    --max-audio-duration-s "$MAX_AUDIO_DURATION_S"
+python -m wyoming_stt_api
